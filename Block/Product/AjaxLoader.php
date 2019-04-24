4196869d746b74a3e957e1ad6e855b819044632d
Richard BAYET <richard.bayet@smile.fr>
2019-04-24 16:42:40 +0200

    Feature Upsell and related products loaded in ajax

---
+++ Block/Product/AjaxLoader.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile ElasticSuite to newer
+ * versions in the future.
+ *
+ *
+ * @category  Smile
+ * @package   Smile\ElasticsuiteRecommender
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2018 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\ElasticsuiteRecommender\Block\Product;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\Url\Helper\Data as UrlHelper;
+
+/**
+ * Ajax recommender loader block.
+ *
+ * @category Smile
+ * @package  Smile\ElasticsuiteRecommender
+ * @author   Richard BAYET <richard.bayet@smile.fr>
+ */
+class AjaxLoader extends Template
+{
+    /**
+     * Core registry
+     *
+     * @var \Magento\Framework\Registry
+     */
+    protected $coreRegistry;
+
+    /**
+     * URL helper
+     *
+     * @var \Magento\Framework\Url\Helper\Data
+     */
+    protected $urlHelper;
+
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context   Template context.
+     * @param \Magento\Framework\Registry                      $registry  Registry.
+     * @param UrlHelper                                        $urlHelper URL helper.
+     * @param array                                            $data      Data.
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        UrlHelper $urlHelper,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->coreRegistry = $registry;
+        $this->urlHelper = $urlHelper;
+    }
+
+    /**
+     * Get encoded URL to redirect to (after compare)
+     *
+     * @return string
+     */
+    public function getRedirectEncodedUrl()
+    {
+        return $this->urlHelper->getEncodedUrl();
+    }
+
+    /**
+     * Get URL for ajax call
+     *
+     * @return string
+     */
+    public function getProductRecommenderUrl()
+    {
+        return $this->getUrl(
+            'elasticsuite/recommender/ajax',
+            [
+                '_secure' => $this->getRequest()->isSecure(),
+                'id' => $this->getProductId(),
+            ]
+        );
+    }
+
+    /**
+     * Get current product id
+     *
+     * @return null|int
+     */
+    private function getProductId()
+    {
+        $product = $this->coreRegistry->registry('product');
+
+        return $product ? $product->getId() : null;
+    }
+}
---
+++ Block/Product/ProductList/Item/AddTo/Compare.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ *
+ * @category  Smile
+ * @package   Smile\Elasticsuite
+ * @author    Richard BAYET <richard.bayet@smile.fr>
+ * @copyright 2019 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+namespace Smile\ElasticsuiteRecommender\Block\Product\ProductList\Item\AddTo;
+
+use Magento\Catalog\Block\Product\ProductList\Item\AddTo\Compare as AddToCompare;
+use Smile\ElasticsuiteRecommender\Helper\Product\Compare as ProductCompareHelper;
+
+/**
+ * Add to compare custom block
+ *
+ * @category Smile
+ * @package  Smile\ElasticsuiteRecommender
+ */
+class Compare extends AddToCompare
+{
+    /**
+     * Constructor
+     *
+     * @param \Magento\Catalog\Block\Product\Context $context       Product context.
+     * @param ProductCompareHelper                   $compareHelper Compare helper.
+     * @param array                                  $data          Data.
+     */
+    public function __construct(
+        \Magento\Catalog\Block\Product\Context $context,
+        ProductCompareHelper $compareHelper,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->_compareProduct = $compareHelper;
+    }
+}
