66df21d3ddefcaf103c444c12d90934283c1d58c
Romain Ruaud <romain.ruaud@smile.fr>
2020-01-17 14:31:53 +0100

    Adding results buffer.

--- view/frontend/react_source/ReactESAutocomplete.js
+++ view/frontend/react_source/ReactESAutocomplete.js
@@ -25,23 +25,34 @@ class ReactESAutocomplete extends Component {
         this.state = {
             value : props.value || '',
             results : [],
+            resultsBuffer : {},
             loading: false,
         };
     }
 
     onChange(event) {
-        const { url, minSearchLength, storeCode, state : {loading} } = this;
+        const { url, minSearchLength, storeCode, state : {loading, resultsBuffer} } = this;
 
         let data = {q: event.target.value};
         if (storeCode !== null) {
             data.__store = storeCode;
         }
-
         let queryString = Object.keys(data).map(key => key + '=' + data[key]).join('&');
+
         this.setState((state) => { return {value: data.q}; });
 
+        // If search is too short, do nothing.
         if (data.q.length < minSearchLength) {
             this.setState((state) => { return { results: []}});
+
+            return;
+        }
+
+        // Compute a cache key for the current (trimmed) input text.
+        let hash = window.btoa(data.q.trim());
+        // Same search has been already processed, serve the results from the cache.
+        if (resultsBuffer[hash] !== undefined) {
+            this.setState((state) => {return {results: resultsBuffer[hash], loading: false};});
 
             return;
         }
@@ -59,8 +70,10 @@ class ReactESAutocomplete extends Component {
             })
             .then(response => response.json())
             .then(responseJson => {
+                // Append results to the cache of previous requests.
+                resultsBuffer[hash] = responseJson;
                 this.setState((state) => {
-                    return {results: responseJson, loading: false};
+                    return {results: responseJson, resultsBuffer: resultsBuffer, loading: false};
                 });
                 console.log(this.state);
             })
