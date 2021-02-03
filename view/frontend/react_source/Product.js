7a587ef8b8e261979b7e75b5ffdaaac6582a1429
Romain Ruaud <romain.ruaud@smile.fr>
2021-02-03 15:39:24 +0100

    Add a way to change the "in" text

--- view/frontend/react_source/Product.js
+++ view/frontend/react_source/Product.js
@@ -10,6 +10,7 @@ class Product extends Component {
         }
 
         this.currencyCode = props.currencyCode || 'USD';
+        this.productInCategoryLabel = props.productInCategoryLabel || "in";
     }
 
     render() {
@@ -25,7 +26,7 @@ class Product extends Component {
                 <a className="instant-search-result" href={'//' + window.location.hostname + '/' + item.url} title={item.name} onMouseDown={(e) => e.preventDefault()}>
                     <div className="thumbnail"><img alt={item.name} src={'//' + window.location.hostname + '/' + item.thumbnail}/></div>
                     <div className="info">{item.name}
-                        <div className="autocomplete-category">in {item.highlightCategory}</div>
+                        <div className="autocomplete-category">{this.productInCategoryLabel + ' ' + item.highlightCategory}</div>
                         <div className="price">
                             <Price currencyCode={this.currencyCode} value={item.price[0].price} />
                         </div>
