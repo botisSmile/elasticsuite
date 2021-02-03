d25719dd62e87efb0a05c564381e933fd2b89a6a
Romain Ruaud <romain.ruaud@smile.fr>
2021-02-03 11:36:55 +0100

    Let the labels be configurable.

--- view/frontend/react_source/Results.js
+++ view/frontend/react_source/Results.js
@@ -5,6 +5,20 @@ import Category from './Category';
 import Term from './Term';
 
 class Results extends Component {
+
+    constructor(props) {
+        super(props);
+
+        if (window && window.ES_REACT_AUTOCOMPLETE_PROPS) {
+            props = {...props, ...window.ES_REACT_AUTOCOMPLETE_PROPS};
+        }
+
+        this.productTitle = props.productTitle || "Products";
+        this.categoryTitle = props.categoryTitle || "Categories";
+        this.termTitle = props.termTitle || "Search terms";
+        this.noResultTitle = props.noResultTitle || "No results";
+    }
+
     componentWillMount() {
         this.setState((state, props) => {
             let items = props.items || false;
@@ -37,9 +51,9 @@ class Results extends Component {
             <div id="search_autocomplete" className="instant-search-result-box" style={{display: ((items.length > 0) && (expanded === true)) ? 'flex' : 'none' }}>
                 <div className="col-3">
                     <dl id="search_autocomplete_term" className="term">
-                        <dt>Search terms</dt>
+                        <dt>{this.termTitle}</dt>
                         {(groupedResults.term === undefined || groupedResults.term.length === 0) &&
-                            <span className="no-results">No results</span>
+                            <span className="no-results">{this.noResultTitle}</span>
                         }
                         {groupedResults.term !== undefined && groupedResults.term.length > 0 &&
                             groupedResults.term.map(function(result, index) {
@@ -50,9 +64,9 @@ class Results extends Component {
                         }
                     </dl>
                 <dl id="search_autocomplete_category" className="category">
-                    <dt>Categories</dt>
+                    <dt>{this.categoryTitle}</dt>
                         {(groupedResults.category === undefined || groupedResults.category.length === 0) &&
-                            <span className="no-results">No results</span>
+                            <span className="no-results">{this.noResultTitle}</span>
                         }
                         {groupedResults.category !== undefined && groupedResults.category.length > 0 &&
                             groupedResults.category.map(function(result, index) {
@@ -65,9 +79,9 @@ class Results extends Component {
                 </div>
                 <div className="col-7">
                     <dl id="search_autocomplete_product" className="product">
-                        <dt>Products</dt>
+                        <dt>{this.productTitle}</dt>
                             {(groupedResults.product === undefined || groupedResults.product.length === 0) &&
-                                <span className="no-results">No results</span>
+                                <span className="no-results">{this.noResultTitle}</span>
                             }
                             {groupedResults.product !== undefined && groupedResults.product.length > 0 &&
                                 groupedResults.product.map(function(result, index) {
