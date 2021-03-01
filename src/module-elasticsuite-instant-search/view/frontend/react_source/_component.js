f2b82e2c97714bca2ae93dedae1e9dece1808a93
Romain Ruaud <romain.ruaud@smile.fr>
2021-03-01 15:55:22 +0100

    Create Instant Search react component only if possible.

--- src/module-elasticsuite-instant-search/view/frontend/react_source/_component.js
+++ src/module-elasticsuite-instant-search/view/frontend/react_source/_component.js
@@ -1,9 +1,10 @@
-const elementId = window.ES_REACT_AUTOCOMPLETE_PROPS.formSelector || 'search_mini_form';
-if (document.getElementById(elementId)) {
-    const element = document.getElementById(elementId);
-    const ReactESAutocomplete = React.lazy(() => import('ReactESAutocomplete'));
-    ReactDOM.render(<React.Suspense fallback={<div dangerouslySetInnerHTML={{__html: element.innerHTML}}/>}>
-        <ReactESAutocomplete originalContent={element.innerHTML} />
-    </React.Suspense>, element);
+if (window.ES_REACT_AUTOCOMPLETE_PROPS && (window.ES_REACT_AUTOCOMPLETE_PROPS.formSelector !== undefined)) {
+    const elementId = window.ES_REACT_AUTOCOMPLETE_PROPS.formSelector || 'search_mini_form';
+    if (document.getElementById(elementId)) {
+        const element             = document.getElementById(elementId);
+        const ReactESAutocomplete = React.lazy(() => import('ReactESAutocomplete'));
+        ReactDOM.render(<React.Suspense fallback={<div dangerouslySetInnerHTML={{__html: element.innerHTML}}/>}>
+            <ReactESAutocomplete originalContent={element.innerHTML}/>
+        </React.Suspense>, element);
+    }
 }
-
