0b18ccc3a695d18efd89ca79ebdc5cd91a2e3b89
Romain Ruaud <romain.ruaud@smile.fr>
2021-03-05 17:01:17 +0100

    Display boost type and proper field labels for behavioral boosts. Fix ESP-158 and Fix ESP-159

--- src/module-elasticsuite-explain/Model/Result/Collector/Optimizers.php
+++ src/module-elasticsuite-explain/Model/Result/Collector/Optimizers.php
@@ -22,6 +22,8 @@ use Smile\ElasticsuiteCore\Api\Search\ContextInterface;
 use Smile\ElasticsuiteCore\Api\Search\Request\ContainerConfigurationInterface;
 use Smile\ElasticsuiteExplain\Model\Result\CollectorInterface;
 use Smile\ElasticsuiteCatalogOptimizer\Model\Optimizer\Collection\ProviderInterface;
+use Smile\ElasticsuiteBehavioralOptimizer\Ui\Component\Optimizer\Source\Config\BehavioralData\Options;
+use Magento\Catalog\Api\ProductAttributeRepositoryInterface;
 
 /**
  * Applied optimizers collector.
@@ -55,17 +57,46 @@ class Optimizers implements CollectorInterface
     private $urlBuilder;
 
     /**
+     * @var ProductAttributeRepositoryInterface
+     */
+    private $attributeRepository;
+
+    /**
+     * @var array behavioral data option-values
+     */
+    private $options = [];
+
+    /**
+     * @var array
+     */
+    private $typeLabels = [];
+
+    /**
      * Optimizers constructor.
      *
-     * @param ProviderInterface          $provider   Optimizers Provider
-     * @param UrlInterface               $urlBuilder URL builder.
-     * @param OptimizerFilterInterface[] $filters    Optimizer filters.
+     * @param ProviderInterface                   $provider                   Optimizers Provider
+     * @param UrlInterface                        $urlBuilder                 URL builder.
+     * @param Options                             $optionsSource              Behavioral Optimizers option source.
+     * @param ProductAttributeRepositoryInterface $productAttributeRepository Attribute Repository
+     * @param OptimizerFilterInterface[]          $filters                    Optimizer filters.
      */
-    public function __construct(ProviderInterface $provider, UrlInterface $urlBuilder, $filters = [])
-    {
-        $this->provider   = $provider;
-        $this->urlBuilder = $urlBuilder;
-        $this->filters    = $filters;
+    public function __construct(
+        ProviderInterface $provider,
+        UrlInterface $urlBuilder,
+        Options $optionsSource,
+        ProductAttributeRepositoryInterface $productAttributeRepository,
+        $filters = []
+    ) {
+        $this->provider            = $provider;
+        $this->urlBuilder          = $urlBuilder;
+        $this->filters             = $filters;
+        $this->attributeRepository = $productAttributeRepository;
+        $this->options             = $optionsSource->toOptionArray();
+        $this->typeLabels          = [
+            'constant_score'  => 'Constant Score',
+            'attribute_value' => 'Based on attribute value',
+            'behavioral'      => 'Based on behavioral data',
+        ];
     }
 
     /**
@@ -94,6 +125,7 @@ class Optimizers implements CollectorInterface
                 'id'        => $optimizer->getId(),
                 'name'      => $optimizer->getName(),
                 'boost'     => $this->getBoost($optimizer),
+                'type'      => $this->getType($optimizer),
                 'tooltip'   => $this->getTooltip($optimizer),
                 'rule'      => nl2br($optimizer->getRuleCondition()->getConditions()->asStringRecursive()),
                 'rule_html' => $optimizer->getRuleCondition()->getConditions()->asHtmlRecursive(),
@@ -121,13 +153,24 @@ class Optimizers implements CollectorInterface
      */
     private function getBoost(OptimizerInterface $optimizer)
     {
-        $result = null;
-
         if ($optimizer->getConfig('constant_score_value')) {
             $result = $optimizer->getConfig('constant_score_value') . '%';
         } elseif ($optimizer->getConfig('scale_factor')) {
-            $field    = $optimizer->getConfig('attribute_code') ?? $optimizer->getConfig('metric') ?? '';
-            $result   = __(sprintf('Proportional to "%s"', $field));
+            $scaleFactor = $optimizer->getConfig('scale_factor');
+            if ($optimizer->getConfig('attribute_code')) {
+                try {
+                    $attribute = $this->getAttribute($optimizer->getConfig('attribute_code'));
+                    $field     = $attribute->getDefaultFrontendLabel();
+                } catch (\Magento\Framework\Exception\NoSuchEntityException $exception) {
+                    $field = $optimizer->getConfig('attribute_code');
+                }
+            } elseif ($optimizer->getConfig('metric')) {
+                $field = $optimizer->getConfig('metric');
+                if (isset($this->options[$field]) && isset($this->options[$field]['label'])) {
+                    $field = $this->options[$field]['label'] ?? $field;
+                }
+            }
+            $result = __(sprintf('%s * %s', $scaleFactor, $field));
         }
 
         return $result;
@@ -145,7 +188,7 @@ class Optimizers implements CollectorInterface
         $result = null;
 
         if ($optimizer->getConfig('constant_score_value')) {
-            $result   = sprintf("1 + (%s / 100)", (float) $optimizer->getConfig('constant_score_value'));
+            $result = sprintf("1 + (%s / 100)", (float) $optimizer->getConfig('constant_score_value'));
         } elseif ($optimizer->getConfig('scale_factor')) {
             $factor   = $optimizer->getConfig('scale_factor') ?? '';
             $modifier = $optimizer->getConfig('scale_function') ?? '';
@@ -154,5 +197,31 @@ class Optimizers implements CollectorInterface
         }
 
         return $result;
+    }
+
+    /**
+     * Get type for a given optimizer.
+     *
+     * @param \Smile\ElasticsuiteCatalogOptimizer\Api\Data\OptimizerInterface $optimizer The optimizer
+     *
+     * @return string
+     */
+    private function getType(OptimizerInterface $optimizer)
+    {
+        $model = $optimizer->getModel();
+
+        return $this->typeLabels[$model] ?? $model ?? '';
+    }
+
+    /**
+     * Get attribute by attribute code.
+     *
+     * @param string $attributeCode The attribute code
+     *
+     * @return \Magento\Catalog\Api\Data\ProductAttributeInterface
+     */
+    private function getAttribute($attributeCode)
+    {
+        return $this->attributeRepository->get($attributeCode);
     }
 }
